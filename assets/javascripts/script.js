3ad0fe7c223d7011dfa14c1f16ced51b9c8c3262
Étienne Gauvin <etienne@gauvin.info>
2016-02-27 16:18:36 +0100

    Mise à jour

---
+++ assets/javascripts/script.js
@@ -0,0 +1,149 @@
+(function (window) {
+  var document = window.document
+
+  window.addEventListener('hashchange', function (event) {
+    var header = document.getElementById('page_header')
+    header.classList.remove('cover')
+  })
+
+  var PostsHandler = function (container) {
+    this.container = container
+
+    window.addEventListener('hashchange', this.reloadFromHash.bind(this))
+  }
+
+  window.PostsHandler = PostsHandler
+
+  PostsHandler.prototype = {
+    load: function () {
+
+      var checkboxes = postsForm.elements['tag']
+      this.checkboxes = []
+      
+      for (var i in checkboxes)
+        if (checkboxes[i].addEventListener)
+          this.checkboxes.push(checkboxes[i])
+
+      this.reloadFromHash()
+      this.displayPosts()
+    },
+
+    /**
+     * Afficher les posts
+     */
+    displayPosts: function () {
+
+      // Affichage de la liste des tags
+      var postsForm = this.container.querySelector('form')
+      var onTagChange = this.onTagChange.bind(this)
+
+      //console.log(postsForm, postsForm.elements, checkboxes)
+
+      for (var i in this.checkboxes) {
+        this.checkboxes[i].addEventListener('change', onTagChange)
+      }
+    },
+
+    /**
+     * Changer les valeurs des checkboxes en fonction du hash
+     */
+    reloadFromHash: function() {
+      var selectedTags = this.getHashTags()
+
+      for (var t = 0; t < this.checkboxes.length; t++) {
+        this.checkboxes[t].checked = ""
+      }
+
+      for (var t = 0; t < this.checkboxes.length; t++) {
+        for (var m = 0; m < selectedTags.length; m++) {
+          if (this.checkboxes[t].value === selectedTags[m]) {
+            console.log(selectedTags[m])
+            this.checkboxes[t].checked = "checked"
+            break
+          }
+        }
+      }
+
+      this.updateVisiblePosts()
+    },
+
+    /**
+     * Retourne les tags présents dans le hash
+     * @return <Array>
+     */
+    getHashTags: function () {
+      var hash = window.location.hash
+      var matches = hash.match(/([a-z0-9._-]+)/g) || []
+
+      for (var m = 0; m < matches.length; m++) {
+        matches[m] = matches[m].replace(/_/g, ' ')
+      }
+
+      return matches
+    },
+
+    /**
+     * Écouteur sur les tags
+     */
+    onTagChange: function (event) {
+      var tags = this.getHashTags()
+      var checkbox = event.target
+
+      console.log(event)
+
+      var found = false
+
+      for (var m = 0; m < tags.length; m++) {
+        if (tags[m] === checkbox.value) {
+          found = true
+
+          if (!checkbox.checked) {
+            tags.splice(m, 1)
+            break
+          }
+        }
+      }
+
+      if (!found && checkbox.checked) {
+        tags.push(checkbox.value)
+      }
+
+      window.location.hash = '#' + tags.join('&').replace(/ /g, '_')
+    },
+
+    /**
+     * Mettre à jour la liste des posts en fonction des tags cliqués
+     */
+    updateVisiblePosts: function () {
+      // Recherche des tags sélectionnés
+      var checkedTags = []
+
+      for (var t = 0; t < this.checkboxes.length; t++) {
+        if (this.checkboxes[t].checked) {
+          checkedTags.push(this.checkboxes[t].value)
+        }
+      }
+
+      // Masquer les posts non concernés
+      var liElements = this.container.querySelectorAll('ol.posts > li')
+
+      for (var l = 0; l < liElements.length; l++) {
+        var li = liElements[l]
+        var liTags = li.dataset.tags.split(', ')
+
+        if (checkedTags.length > 0) {
+          for (var ct = 0; ct < checkedTags.length; ct++) {
+            if (liTags.includes(checkedTags[ct])) {
+              li.classList.remove('hidden')
+              break
+            } else {
+              li.classList.add('hidden')
+            }
+          }
+        } else {
+          li.classList.remove('hidden')
+        }
+      }
+    }
+  }
+})(window)
---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	tumblr: {
+		baseHostname: 'portfolio.etienne.gauvin.info',
+		apiKey: 'Fj4Ebl7c1MydVw29G3d1s9DadIIPVeIG1xqC4uS9RJKVgS8b1Y'
+	},
+
+	downloadData: false,
+
+	paths: {
+		build: '.',
+		templates: 'src/templates',
+		data: 'src/data.json',
+
+		less: 'src/style.less',
+		css: 'assets/stylesheets/style.css'
+	}
+}
