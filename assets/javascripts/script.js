848791c565eba5339bc6a9728673819a4b367f67
Étienne Gauvin <etienne@gauvin.info>
2016-03-07 04:26:18 +0100

    Ajout d'un article

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,10 +1,36 @@
 (function (window) {
   var document = window.document
-
-  window.addEventListener('hashchange', function (event) {
-    var header = document.getElementById('page_header')
-    header.classList.remove('cover')
-  })
+  
+  // Polyfill Array.includes
+	if (!Array.prototype.includes) {
+	  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+	    'use strict';
+	    var O = Object(this);
+	    var len = parseInt(O.length) || 0;
+	    if (len === 0) {
+	      return false;
+	    }
+	    var n = parseInt(arguments[1]) || 0;
+	    var k;
+	    if (n >= 0) {
+	      k = n;
+	    } else {
+	      k = len + n;
+	      if (k < 0) {k = 0;}
+	    }
+	    var currentElement;
+	    while (k < len) {
+	      currentElement = O[k];
+	      if (searchElement === currentElement ||
+	         (searchElement !== searchElement && currentElement !== currentElement)) {
+	        // NaN !== NaN
+	        return true;
+	      }
+	      k++;
+	    }
+	    return false;
+	  };
+	}
 
   var PostsHandler = function (container) {
     this.container = container
